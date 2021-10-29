dd78c966da42d55f9a5edb3ebac751cba29f3974
MuhamadKukuh <mkukuhsetiadi@gmail.com>
2021-10-29 13:11:02 +0700

    first comit

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php 
+  include 'koneksi.php';
+
+ ?>
+<!DOCTYPE html>
+<html>
+	<?php include 'head.php'; ?>
+<body class="hold-transition sidebar-mini layout-fixed">
+
+  	
+    <?php include 'header.php';
+          include 'sidebar.php';
+    ?>
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Dashboard</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard v1</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3><?php $data = mysqli_query($koneksi, "SELECT * FROM users WHERE id_role= 3"); 
+                $hitung = mysqli_num_rows($data);
+                echo $hitung;
+                ?>
+                </h3>
+
+                <p>Student's</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>$ 150</h3>
+
+                <p>Saldo</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-cash"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>150
+                </h3>
+
+                <p>Total Use</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    </div>
+
+<!-- js -->
+<?php include 'js.php'; ?>
+</body>
+</html>
